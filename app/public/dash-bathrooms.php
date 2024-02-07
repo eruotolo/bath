da8b2ca5662c7207e814762e5fd2269b1c9832e9
eruotolo <edgardoruotolo@outlook.com>
2024-02-07 13:13:14 -0300

    Acción: Cambios en el Script Edit. Fecha: 05-02-2024 Versión: 4.2

--- app/public/dash-bathrooms.php
+++ app/public/dash-bathrooms.php
@@ -203,7 +203,29 @@
 <script src="assets/js/pages/datatables.init.js"></script>
 
 <script src="assets/js/app.js"></script>
-<script src="assets/js/bathEdit.js"></script>
+<!--<script src="assets/js/bathEdit.js"></script>-->
+<script>
+	$(document).on('click', '.editar', function () {
+		let id_Bath = $(this).data('id');
+		$.ajax({
+			url: 'controller/bath-get.php',
+			type: 'post',
+			data: { id_Bath: id_Bath },
+			dataType: 'text',
+			success: function (response) {
+				let bathData = JSON.parse(response);
+				$('#idBath').val(bathData.id_Bath);
+				$('#codigoBath').val(bathData.codigo_Bath);
+				$('#fechaCompraBath').val(bathData.fechaCompra_Bath);
+				$('#observacionBath').val(bathData.observacion_Bath);
+				$('#estadoBath').val(bathData.estado_Bath);
+			},
+			error: function (request, status, error) {
+				$('.modal-body').html(request.responseText);
+			},
+		});
+	});
+</script>
 
 </body>
 
